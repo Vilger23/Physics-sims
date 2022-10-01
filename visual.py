39c9386f610575647ba2ea159438747649492cf5
Vilger23 <59168174+Vilger23@users.noreply.github.com>
2022-10-01 10:49:00 +0200

    Update visual.py

--- visual.py
+++ visual.py
@@ -3,6 +3,7 @@ from functools import reduce
 import numpy as np 
 from PIL import Image, ImageTk
 import numbers
+import math
 
 class Light:
     P = None
@@ -76,7 +77,7 @@ class Sphere:
         P_rf = origin + Vect*t
         N = (P_rf-self.center).normalized()
         L = (Light.P - P_rf).normalized()  
-        I = (Cam - P_rf).normalized()
+        I = (cam.pos - P_rf).normalized()
         color = self.ambient
         epsilon = 0.00001
         nudged = P_rf + L*epsilon
@@ -111,54 +112,98 @@ def trace(scene, origin, Vect, reflections = 0):
             color += pC.set_rgb(seen)
     return color
 
+class Cam:
+    def __init__(self, origin, V_N, width, aspect_ratio, FOV):
+        self.origin = origin
+        self.width = width
+        self.r = aspect_ratio[0]/aspect_ratio[1]
+        self.height = int(width/self.r)
+
+        self.FOV = FOV
+
+    def configure_cam(self):
+        self.V_N = (earth.center - self.origin).normalized()
+        Vx, Vy, Vz = self.V_N.xyz()
+        theta = math.acos(Vx)
+        deg = math.pi/2
+
+        inner = math.cos(deg-math.acos(Vy)) if Vy < 0 else math.sin(deg-math.acos(Vy))
+
+        rotationV = v(math.cos(deg-math.acos(Vx)), inner, 0)
+        rotationV.normalized()
+        
+
+        tiltVz = math.sin(deg+math.asin(Vz))*(1/self.r)
+        tiltVx = math.sqrt((1/self.r)**2 - tiltVz**2)*math.cos(theta)
+        tiltVy = math.sqrt(1-tiltVx**2-tiltVz**2)
+
+        tiltV = v(tiltVx, tiltVy, tiltVz)
+        print(rotationV.xyz())
+        print(rotationV.dot(self.V_N))
+        topleft = tiltV - rotationV          + self.origin
+        topright = tiltV + rotationV         + self.origin
+        bottomleft = tiltV*(-1) - rotationV  + self.origin
+        bottomright = tiltV*(-1) + rotationV + self.origin
+
+
+
+        ox = np.linspace(topleft.x, bottomleft.x, self.height).reshape(-1, 1)
+        oy = np.linspace(topleft.y, bottomleft.y, self.height).reshape(-1, 1)
+        oz = np.linspace(topleft.z, bottomleft.z, self.height).reshape(-1, 1)
+
+        tx = np.linspace(topright.x, bottomright.x, self.height).reshape(-1, 1)
+        ty = np.linspace(topright.y, bottomright.y, self.height).reshape(-1, 1)
+        tz = np.linspace(topright.z, bottomright.z, self.height).reshape(-1, 1)
+        c = np.linspace(0, 1, self.width)
+        self.x = ox + (tx - ox) * c
+        self.y = oy + (ty - oy) * c
+        self.z = oz + (tz - oz) * c
+
+        dist = 1/math.tan(self.FOV/2)
+        self.pos = self.V_N*-dist
+
+
+    def render(self):
+        self.configure_cam()
+        Vect = (v(self.x, self.y, self.z) - self.pos).normalized()
+        color = trace(scene, self.pos, Vect)
+        rgb = [Image.fromarray((255*np.clip(c, 0, 1).reshape((height, width))).astype(np.uint8), "L") for c in color.xyz()]
+        img = Image.merge("RGB", rgb).resize((int(width/2), int(height/2)))
+        IMAGE = ImageTk.PhotoImage(image=img)
+
+        label.config(image=IMAGE)
+        label.pack()
+        root.update()
+
+
 Light.P = v(5, 5, 0)
 to_far = 10.0**39
-Cam = v(0, 0, -2) 
 
 width = 400
 aspect_ratio = 4, 4
 r = aspect_ratio[0]/aspect_ratio[1]
 height = int(width/r)
 
-'''scene = [
-    Sphere(2, v(0, 0, 10), v(1, 0.8, 0), 100, 0.5),
-    CheckeredPlane( v(0, -2, 0), v(0, -1, 0), v(0.7, 0.7, 0.7), 0),
-    #Plane( v(0, 0, 20), v(0, 0, 1), v(0.4, 0.4, 0.8), 0),
-    #Plane( v(0, 0, -5), v(0, 0, -1), v(0.8, 0.4, 0.4), 0),
-    #Plane( v(5, 0, 0), v(1, 0, 0), v(0.4, 0.8, 0.4), 0),
-    #Plane( v(-5, 0, 0), v(-1, 0, 0), v(0.4, 0.4, 0.8), 0),
-    #Plane( v(0, 7, 0), v(0, 1, 0), v(0.8, 0.8, 0.4), 0)
-]'''
-
 scene = [
-    Sphere(2, v(0, 0, 10), v(1, 0.8, 0), 100, 0.5),
+    Sphere(2, v(15, 0, 0), v(1, 0.8, 0), 20, 0),
+
+    Sphere(1, v(10, 0, 0), v(0, 0.3, 0.9), 100, 0, v(0,0,0))
 ]
+sun, earth = scene
 
-
-
-def render():
-    x = np.tile(np.linspace(-1, 1, width), height)
-    y = np.repeat(np.linspace(1/r, -1/r, height), width)
-    Vect = (v(x, y, 0) - Cam).normalized()
-    color = trace(scene, Cam, Vect)
-    rgb = [Image.fromarray((255*np.clip(c, 0, 1).reshape((height, width))).astype(np.uint8), "L") for c in color.xyz()]
-    img = Image.merge("RGB", rgb).resize((int(width/2), int(height/2)))
-    IMAGE = ImageTk.PhotoImage(image=img)
-
-    label.config(image=IMAGE)
-    label.pack()
-    root.update()
+cam = Cam(v(0, 0, 0), v(1, 0, 0), 400, (4,4), math.pi/3)
 
 root = tk.Tk()
 label = tk.Label(root)
-render()
-frames = 50
+cam.render()
 i=0
+frames = 50
 while True:
     i += 1
     theta = np.pi*2/frames
-    Light.P = v(np.cos(theta*i)*6, 2, np.sin(theta*i)*6+10)
-    render()
-    
+    Light.P = (cam.pos-sun.center).normalized()*(sun.r+1)
+    earth.center = v(np.cos(theta*i)*5+15,np.sin(theta*i)*5, 0)
+    cam.render()
+
 root.bind('<Button-1>', lambda x : mouseclick(x, scene))
 root.mainloop()
